c989f8502478c1fd39b298d74caf90ac58f9b7f2
tehseen1819 <tehseenahmad005@gmail.com>
2026-06-04 21:51:33 +0530

    setContext

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,12 @@
 import { Stack } from "expo-router";
+import { CartProvider } from "./cartContext";
+
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <CartProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </CartProvider>
   );
 }
